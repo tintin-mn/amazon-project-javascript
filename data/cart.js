e2589bf18fd3ffc92726975fcead167bbeeed1af
lenovo <tina1377mazikian@gmail.com>
2024-12-09 18:29:57 +0330

    complete checkout page javascript

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,26 @@
-export const cart = [
-    {
-        productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-        quantity: 2,
-    },
-    {
-        productId: '3fdfe8d6-9a15-4979-b459-585b0d0545b9',
-        quantity: 1,
-    }
-]
+export let cart =
+    JSON.parse(localStorage.getItem('cart'))
+
+if (!cart) {
+    cart =
+        [
+            {
+                productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+                quantity: 2,
+                deliveryOptionId: '1'
+            },
+            {
+                productId: '3fdfe8d6-9a15-4979-b459-585b0d0545b9',
+                quantity: 1,
+                deliveryOptionId: '2'
+            }
+        ]
+}
+
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart))
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -24,6 +37,34 @@ export function addToCart(productId) {
         cart.push({
             productId,
             quantity: 1,
+            deliveryOptionId: '1'
         })
     }
+    saveToStorage()
+}
+
+export function removeFromCart(productId) {
+    const newCart = []
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem)
+        }
+    })
+    cart = newCart
+
+    saveToStorage()
+}
+
+export function updateDeliveryOption(productId, deliveryOptionId) {
+    let matchingItem;
+
+    cart.forEach((cartItem) => {
+        if (productId === cartItem.productId) {
+            matchingItem = cartItem;
+        }
+    })
+
+    matchingItem.deliveryOptionId = deliveryOptionId
+
+    saveToStorage()
 }
