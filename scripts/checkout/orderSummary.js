62f0abef633f707263b1c5752b53e07576062620
lenovo <tina1377mazikian@gmail.com>
2024-12-09 18:34:31 +0330

    fix some bugs in updating prices

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -4,6 +4,7 @@ import {formatCurrency} from "../utils/money.js";
 import {deliveryOptions, getDeliveryOption} from "../../data/deliveryOptions.js";
 import {hello} from 'https://unpkg.com/supersimpledev@1.0.1/hello.esm.js';
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
+import {renderPaymentSummary} from "./paymentSummary.js";
 
 hello();
 
@@ -122,6 +123,8 @@ export function renderOrderSummary() {
             removeFromCart(productId)
             const container = document.querySelector(`.js-cart-item-container-${productId}`)
             container.remove()
+
+            renderPaymentSummary()
         })
 
     })
@@ -131,6 +134,7 @@ export function renderOrderSummary() {
             const {productId, deliveryOptionId} = element.dataset
             updateDeliveryOption(productId, deliveryOptionId)
             renderOrderSummary()
+            renderPaymentSummary()
         })
     })
 }
